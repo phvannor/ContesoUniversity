8cab36b731b2d21ead460968423ffc478a5d170a
Shrirang Shirodkar <shrirs@microsoft.com>
2015-07-24 13:35:06 -0700

    Add pull method to proxy

--- client/iOS/ContosoUniversity/ContosoUniversity/proxies/CourseProxy.m
+++ client/iOS/ContosoUniversity/ContosoUniversity/proxies/CourseProxy.m
@@ -8,4 +8,29 @@
 
 #import "CourseProxy.h"
 
+@implementation CourseProxy
 
+@synthesize table = _table;
+
+- (instancetype)initWithMobileAppClient:(MSClient *)client tableName:(NSString *)tableName {
+   
+    self = [super init];
+    
+    if (self) {
+        _client = client;
+        _table = [client syncTableWithName:tableName];
+    }
+    
+    return self;
+}
+
+- (void)insert:(CourseModel *)course completion:(MSSyncItemBlock)completion {
+    
+    [self.table insert:course.dictionary completion:completion];
+}
+
+- (void)pullData:(MSSyncBlock)completion {
+    [self.table pullWithQuery:[self.table query] queryId:nil completion:completion];
+}
+
+@end
