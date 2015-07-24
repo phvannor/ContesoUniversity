8f3037d389ac532799cc2432ff44b8edff13d81c
Phillip Van Nortwick <phvannor@microsoft.com>
2015-07-24 13:45:15 -0700

    finish table controllers

---
+++ client/iOS/ContosoUniversity/ContosoUniversity/Instructors/InstructorsTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  InstructorsTableViewController.h
+//  ContosoUniversity
+//
+//  Created by Phillip Van Nortwick on 7/24/15.
+//  Copyright (c) 2015 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface InstructorsTableViewController : UITableViewController<NSFetchedResultsControllerDelegate>
+
+@end
---
+++ client/iOS/ContosoUniversity/ContosoUniversity/Students/StudentsTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  StudentsTableViewController.h
+//  ContosoUniversity
+//
+//  Created by Phillip Van Nortwick on 7/24/15.
+//  Copyright (c) 2015 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface StudentsTableViewController : UITableViewController<NSFetchedResultsControllerDelegate>
+
+@end
